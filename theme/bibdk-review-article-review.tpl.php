78529a365d7a41695962029b6264449ec85b143e
Søren Vibjerg <vibjerg@gmail.com>
2013-09-04 11:14:14 +0200

    add work tab for newspaper reviews

---
+++ theme/bibdk-review-article-review.tpl.php
@@ -0,0 +1,10 @@
+<div class='bibdk-article-review clearfix' >
+  <div class='openformat-info'>
+  <b><?php print $title ?></b> <?php print $publisher ?> <br/>
+    <?php print $creator ?> <br/>
+      <?php print drupal_render($rating) ?>
+  </div>
+  <div class='bibdk-review-link'>
+    <?php print $link ?>
+</div>
+  </div>
