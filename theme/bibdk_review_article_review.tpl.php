0aa899f224a49ec547d5f0a9cd152a064c4e4c29
JrgenGNielsen <jgn@dbc.dk>
2014-08-01 12:42:57 +0200

    style review and netarchive links

--- theme/bibdk_review_article_review.tpl.php
+++ theme/bibdk_review_article_review.tpl.php
@@ -1,11 +1,11 @@
 <div class='bibdk-article-review clearfix' >
   <div class='openformat-info'>
-  <b><?php print $title ?></b> <?php print $publisher ?> <br/>
+    <b><?php print $title ?></b> <?php print $publisher ?> <br/>
     <?php print $creator ?> <br/>
-      <?php print drupal_render($rating) ?>
+    <?php print drupal_render($rating) ?>
   </div>
   <div class='bibdk-review-link'>
-    <?php print $link ?>
+    <?php print drupal_render($link); ?>
   </div>
 <?php if ( !empty($netarchive) ) { ?>
   <div class='bibdk-review-link'>
